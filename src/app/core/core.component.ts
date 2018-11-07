7db5a7c570b4228e11664fc2244854e6a5056525
manishkirange <manishkirange82@gmail.com>
2018-11-08 00:04:33 +0530

    client-a files

---
+++ src/app/core/core.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  template: `
+    <div id="client-a">
+      <img src="client-a/favicon.ico" />
+      <br>
+      <div class="card">
+        <div class="content">
+          <a routerLink="page1">Flight Search</a> | <a routerLink="page2">Advanced</a>
+        </div>
+      </div>
+      <router-outlet></router-outlet>
+    </div>
+  `,
+  styles: [`
+    #client-a { border: darkred dashed 5px; padding: 10px }
+  `],
+})
+export class CoreComponent {
+}
